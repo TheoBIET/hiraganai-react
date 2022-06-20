9a4025559ee6a7ab429a3bd6aca5793e04addf52
TheoBIET <dev.theobiet@gmail.com>
2022-06-20 15:01:44 +0200

    :tada: | Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef } from "react";
+import { FaEraser, FaUndo } from "react-icons/fa";
+
+import CanvasDraw from "react-canvas-draw";
+import axios from "axios";
+import DataURIToBlob from "./utils/blob";
 
 function App() {
+  const BASE_URL = "http://localhost:8080/api/v1";
+  const PREDICT_URL = `${BASE_URL}/predict`;
+  const canvasDraw = useRef(null);
+
+  const undo = () => {
+    canvasDraw.current.undo();
+  };
+
+  const clear = () => {
+    canvasDraw.current.clear();
+  };
+
+  const getPrediction = async () => {
+    const base64 = canvasDraw.current.getDataURL("image/png", "", "#ffffff");
+    const file = DataURIToBlob(base64);
+    const formData = new FormData();
+    formData.append("file", file);
+
+    const response = await axios.post(PREDICT_URL, formData);
+    const { data } = response;
+    const { prediction } = data;
+    const [phonetic, hiragana, percentage] = prediction;
+    return { phonetic, hiragana, percentage };
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Nihongo</h1>
+      <p>Learn Japanese Alphabet with AI</p>
+      <div class="container">
+        <div className="container__drawer">
+          <CanvasDraw ref={canvasDraw} brushColor="black" lazyRadius={0} />
+        </div>
+        <div className="">
+          <div className="is-flex">
+            <div className="button is-info" onClick={undo}>
+              <FaUndo />
+            </div>
+            <div className="button is-danger" onClick={clear}>
+              <FaEraser />
+            </div>
+          </div>
+          <div className="button is-warning" onClick={getPrediction}>
+            Make Prediction
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
